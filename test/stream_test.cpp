6e7f5aeffc4e8e139747e29c39f7fba78a3b2456
alex <alex@lucca>
2016-10-10 20:07:33 -0400

    add gzip test

--- test/stream_test.cpp
+++ test/stream_test.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
+
 #include <gtest/gtest.h>
 
 #include "streams/gzip.h"
 
 TEST(gzip_stream, tests) {
  atr::streams::gzip gzip_handle; 
+ ASSERT_EQ(gzip_handle.open("./test/data/data.csv.gz"), true);
+
+ char buffer[1000];
+ auto read_size = gzip_handle.read(buffer, sizeof(buffer));
+ ASSERT_EQ(read_size > 0, true); 
+ ASSERT_EQ(read_size, 21);
+ auto string_result = std::string(buffer, read_size);
+ ASSERT_EQ(string_result, "test,hey\n1232,sdfdsf\n");
+ ASSERT_EQ(gzip_handle.close(), true);
 }
 
 int main(int argc, char **argv) {
